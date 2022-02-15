4a515619a0afae0b0de547fe48072986b1ea27c6
Sheeka Patak <sheeka.patak@gmail.com>
2022-02-15 17:27:33 +0000

    context and seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,24 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias BookStore.Store
+
+titles = elem(File.read("priv/data/book_titles.txt"), 1) |> String.split("\n")
+genres = elem(File.read("priv/data/genres.txt"), 1) |> String.split("\n")
+
+Enum.each(1..50, fn _x ->
+  title = Enum.random(titles)
+  genre = Enum.random(genres)
+  price = String.to_float("#{Enum.random(1..200)}.#{Enum.random(1..99)}")
+
+  {:ok, _book} = Store.create_book(%{
+    author_first_name: Faker.Person.first_name,
+    author_last_name: Faker.Person.last_name,
+    description: Faker.Lorem.Shakespeare.En.hamlet(),
+    genre: genre,
+    isbn: Faker.Code.isbn(),
+    price: price,
+    title: title,
+    year_published: Enum.random(1500..2022)
+  })
+end)
