27bc49c5a1bf0bc684561fe20cc931396953ee8d
Sheeka Patak <sheeka.patak@gmail.com>
2022-02-16 12:01:46 +0000

    pass keyword list to #assign

--- lib/book_store_web/live/admin_live.ex
+++ lib/book_store_web/live/admin_live.ex
@@ -2,14 +2,20 @@ defmodule BookStoreWeb.AdminLive do
   use BookStoreWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, :book_count, 10)
+    socket =
+      assign(socket,
+        avg_price_per_genre: 10.00,
+        num_books: 200,
+        num_authors: 12,
+        avg_books_per_author: 2
+      )
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
     <h1>Admin</h1>
-    <%= @book_count %>
+    <%= @num_books %>
     """
   end
 end
