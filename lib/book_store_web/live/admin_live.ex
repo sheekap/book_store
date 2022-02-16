8839d278c9f356c0554b778ace076f09dfaf1898
Sheeka Patak <sheeka.patak@gmail.com>
2022-02-16 13:31:24 +0000

    add event handling to refresh button

--- lib/book_store_web/live/admin_live.ex
+++ lib/book_store_web/live/admin_live.ex
@@ -4,11 +4,7 @@ defmodule BookStoreWeb.AdminLive do
   alias BookStore.Store
 
   def mount(_params, _session, socket) do
-    socket =
-      assign(socket,
-        num_books: Enum.count(Store.list_books),
-        num_authors: Enum.count(Store.list_authors)
-      )
+    socket = assign_counts(socket)
     {:ok, socket}
   end
 
@@ -26,8 +22,20 @@ defmodule BookStoreWeb.AdminLive do
       </div>
     </div>
     <div>
-    <button>Refresh</button>
+    <button phx-click="refresh">Refresh</button>
     </div>
     """
+  end
+
+  def handle_event("refresh", _, socket) do
+    socket = assign_counts(socket)
+    {:noreply, socket}
+  end
+
+  defp assign_counts(socket) do
+    assign(socket,
+    num_books: Enum.count(Store.list_books),
+    num_authors: Enum.count(Store.list_authors)
+    )
   end
 end
